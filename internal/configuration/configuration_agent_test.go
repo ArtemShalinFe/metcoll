df78838d89884ccd1224858ad835861cb1ef8b3b
Artem Shalin <a.shalin@rtinform.ru>
2023-08-21 21:32:21 +0300

    add new tests

--- internal/configuration/configuration_agent_test.go
+++ internal/configuration/configuration_agent_test.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"reflect"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -54,6 +55,69 @@ func TestCheckConfigPriority(t *testing.T) {
 			}
 			assert.Equal(t, tt.want.Server, got.Server)
 			assert.Equal(t, tt.want.Limit, got.Limit)
+		})
+	}
+}
+
+func TestConfigAgent_String(t *testing.T) {
+	type fields struct {
+		Server         string
+		Key            []byte
+		PollInterval   int
+		ReportInterval int
+		Limit          int
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		want   string
+	}{
+		{
+			name: "check print hashkey",
+			fields: fields{
+				Server:         "nope",
+				Key:            []byte("testKey"),
+				PollInterval:   1,
+				ReportInterval: 1,
+				Limit:          1,
+			},
+			want: "Addres: nope, ReportInterval: 1, PollInterval: 1, Limit: 1",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			c := &ConfigAgent{
+				Server:         tt.fields.Server,
+				Key:            tt.fields.Key,
+				PollInterval:   tt.fields.PollInterval,
+				ReportInterval: tt.fields.ReportInterval,
+				Limit:          tt.fields.Limit,
+			}
+			if got := c.String(); got != tt.want {
+				t.Errorf("ConfigAgent.String() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestParseAgent(t *testing.T) {
+	tests := []struct {
+		name    string
+		want    *ConfigAgent
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := ParseAgent()
+			if (err != nil) != tt.wantErr {
+				t.Errorf("ParseAgent() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("ParseAgent() = %v, want %v", got, tt.want)
+			}
 		})
 	}
 }
